ef710bf3d55f736aebbbfb0475597ce510e6a2c8
treecaptcha <pie96464@gmail.com>
2021-05-26 09:44:18 -0400

    reeeeeee

--- buildMAC.sh
+++ buildMAC.sh
@@ -1,2 +1,2 @@
-py2applet --make-setup main.py bagg.png france.png background.png licences.txt
+py2applet --iconfile icon.icns --make-setup main.py  bagg.png france.png background.png licences.txt
 python setup.py py2app
